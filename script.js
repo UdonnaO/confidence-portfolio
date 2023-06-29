2f9f6219e83b6e5b10f3a05a56dff2b499d3438e
UdonnaO <94331001+UdonnaO@users.noreply.github.com>
2023-06-29 08:46:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
+let burger = document.querySelector('.burger');
 let isClicked = true;
 const readMoreContent = document.querySelector(".read-more")
 const readMore = document.querySelector(".btn-read-more");
+const menu = document.querySelector('.menu');
 
 readMore.addEventListener("click", reveal);
 
@@ -13,6 +15,18 @@ function reveal(){
     }else {
         readMoreContent.style.display = "none";
         readMore.innerHTML = "Read more";
+        isClicked = true;
+    }
+}
+
+burger.addEventListener("click", revealMenu);
+
+function revealMenu(){
+    if (isClicked) {
+        menu.style.display = "inline";
+        isClicked = false;
+    }else {
+        menu.style.display = "none";
         isClicked = true;
     }
 }
